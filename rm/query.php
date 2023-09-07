027add578a43a710c6e67851f3005050e5ecec83
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-07 07:45:39 -0400

    $1

--- rm/query.php
+++ rm/query.php
@@ -14,19 +14,48 @@ try {
 
 $lite->createCollation('RMNOCASE', 'strnatcmp');	
 
-$q = file_get_contents("media.sql");
+function fetch_media(SQLite3 $lite) : SQLite3Result
+{
+  $media_query = file_get_contents("media.sql");
 
-$returned_set = $lite->query($q);
+  return $lite->query($media_query);
+}
+
+function fetch_factTypes(SQLite3 $lite) : array
+{
+  static $query = "select FactTypeID as id, Name from FactTypeTable";
+
+  $result = $lite->query($query);
+
+  $output = array();
+
+  while($row = $result->fetchArray(SQLITE3_ASSOC))
+      
+     $output[$row['id']] = $row['Name'];
+
+  return $output;
+}
+
+$factTypes = fetch_factTypes($lite);
+
+$media_result = fetch_media($lite);
 
 $cols = array("MediaType", 'MediaPath', 'MediaFile', 'OwnerTypeDesc', 'OwnerName','MediaDate');
 
-while($result = $returned_set->fetchArray(SQLITE3_ASSOC)) {
+while($media_row = $media_result->fetchArray(SQLITE3_ASSOC)) {
 
+     /*
      foreach($cols as $attrib) {
-    
-       echo $attrib . " = " . $result[$attrib] . "\n";
+              
+        
+       echo $attrib . " = " . $media_row[$attrib] . "\n";
      }
      
+     //  todo: Use the Fact/Event Type id to look up the FactType Name
+   
+     $factType['MediaType']
+     */
+     print_r($medida_row);
      echo "------------------------\n";
 }
 
