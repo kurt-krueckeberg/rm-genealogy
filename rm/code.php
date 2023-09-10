c225f4ac5be42dafe204573dde56c3c467d2e20f
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-10 18:39:55 -0400

    $1

--- rm/code.php
+++ rm/code.php
@@ -5,5 +5,6 @@ $regex = '/([A-ZÄÖÜ]+), (?:([A-ZÄÖÜ][a-zöäü]+) *){1,}-\d+/';
 
 $regex = '/^OwnerName = ([A-ZÄÖÜ]+), (?:([^ ]+) *){1,}-\d+/'
 
+// This is the sim[lest regex but it still misses a few lines
 $regex = '/^OwnerName = ([A-ZÄÖÜü]+),([^-]+)-\d+/'; // <-- still misses some lines--why?
 
