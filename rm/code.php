6fdd6717bab9bfecb5ba3823d0ca24b2fc06652a
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-10 19:04:00 -0400

    $1

--- rm/code.php
+++ rm/code.php
@@ -1,10 +1,8 @@
 <?php
 
-// ALSBERMEYER, 
-$regex = '/([A-ZÄÖÜ]+), (?:([A-ZÄÖÜ][a-zöäü]+) *){1,}-\d+/'; 
-
-$regex = '/^OwnerName = ([A-ZÄÖÜ]+), (?:([^ ]+) *){1,}-\d+/'
-
-// This is the sim[lest regex but it still misses a few lines
-$regex = '/^OwnerName = ([A-ZÄÖÜü]+),([^-]+)-\d+/'; // <-- still misses some lines--why?
+/*
+ This misses lines where there is no surname.
+ How can they have happened?
+ */
+$regex = '/^OwnerName = ([^,]+),([^-]+)-\d+/'; 
 
