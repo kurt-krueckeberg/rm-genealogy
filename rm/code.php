b37610b33bab17814fd58fa26f117deaad5a25cd
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-10 18:36:47 -0400

    $1

---
+++ rm/code.php
@@ -0,0 +1,9 @@
+<?php
+
+// ALSBERMEYER, 
+$regex = '/([A-ZÄÖÜ]+), (?:([A-ZÄÖÜ][a-zöäü]+) *){1,}-\d+/'; 
+
+$regex = '/^OwnerName = ([A-ZÄÖÜ]+), (?:([^ ]+) *){1,}-\d+/'
+
+$regex = '/^OwnerName = ([A-ZÄÖÜü]+),([^-]+)-\d+/'; // <-- still misses some lines--why?
+
