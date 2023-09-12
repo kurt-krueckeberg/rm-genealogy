f8d9252827c15915a923ae8bc0964d05d90f80fe
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-12 16:12:29 -0400

    $1

--- rm/code.php
+++ rm/code.php
@@ -2,33 +2,48 @@
 declare(strict_types=1);
 use RootsMagic\FileReader;
 
+include 'vendor/autoload.php';
+
 /*
  This misses lines where there is no surname.
  How can they have happened?
  */
-class Functor {
 
-  public function __construct()
+class Functor
+{
+
+  private readonly string $regex;
+  private string $currentImage;
+  
+  public function __construct(string $regularEx)
   {
+    $this->regex = $regularEx;
   }
 
-  public function __invoke(array $matches)
+  public function __invoke(string $line)
   {
+    if (strcmp(substr($line, 0, 8), "MediaFile") === 0) {
 
-     if (preg_match($regex, $line, $matches) !== 1) 
-       return;
-     
-     echo "Surname, given: " . $matches[1] . ", " . $matches[2] . "\n";
+      $this->currentImage = substr($line, 13);
+      return;
+    }
+       
+    if (preg_match($this->regex, $line, $matches) !== 1)
+      return;
+
+    echo "Surname, given: " . $matches[1] . ", " . $matches[2] . "\n";
+
+    $this->currentImage = '';
   }
 }
 
-$regex = '/^OwnerName = ([^,]+),([^-]+)-\d+/'; 
+$regex = '/^OwnerName = ([^,]+),([^-]+)-\d+/';
 
-$functor = new Functor();
+$functor = new Functor($regex);
 
 $file = new FileReader('output.txt');
 
-foreach($file as $no =< $line) {
+foreach ($file as $no => $line) {
 
-  $functor($matches);
+  $functor($line);
 }
