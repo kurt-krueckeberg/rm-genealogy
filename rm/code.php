d22b3a8976c66054d0ee624196ab5eceb6858769
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-12 16:30:57 -0400

    $1

--- rm/code.php
+++ rm/code.php
@@ -9,41 +9,26 @@ include 'vendor/autoload.php';
  How can they have happened?
  */
 
-class Functor
-{
 
-  private readonly string $regex;
-  private string $currentImage;
-  
-  public function __construct(string $regularEx)
-  {
-    $this->regex = $regularEx;
-  }
-
-  public function __invoke(string $line)
-  {
-    if (strcmp(substr($line, 0, 8), "MediaFile") === 0) {
-
-      $this->currentImage = substr($line, 13);
-      return;
-    }
-       
-    if (preg_match($this->regex, $line, $matches) !== 1)
-      return;
-
-    echo "Surname, given: " . $matches[1] . ", " . $matches[2] . "\n";
-
-    $this->currentImage = '';
-  }
-}
-
-$regex = '/^OwnerName = ([^,]+),([^-]+)-\d+/';
+//--$regex = '/^OwnerName = ([^,]+),([^-]+)-\d+/';
 
 $functor = new Functor($regex);
 
 $file = new FileReader('output.txt');
 
+$regex = '/([^,]+),([^-]+)-\d+/';
+
 foreach ($file as $no => $line) {
 
-  $functor($line);
+  if (strcmp(substr($line, 0, 8), "MediaFile") === 0) 
+
+    $image = substr($line, 13);
+  
+  else if ((strcmp(substr($line, 0, 9), "OwnerName") {
+
+    // choose substring where the surname begins   
+    $x = substr($line, 13);
+
+    if (preg_match($regex, $line, $matches) !== 1)
+  }
 }
