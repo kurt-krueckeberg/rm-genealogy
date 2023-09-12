e2f2e2a20ae0af14604febe012c374c733f37ef1
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-12 12:02:19 -0400

    $1

--- rm/code.php
+++ rm/code.php
@@ -1,8 +1,32 @@
 <?php
+declare(strict_types=1);
 
 /*
  This misses lines where there is no surname.
  How can they have happened?
  */
+class Functor {
+
+  public function __invoke(array $matches)
+  {
+
+     if (preg_match($regex, $line, $matches) !== 1) 
+       return;
+ 
+
+  }
+}
+
 $regex = '/^OwnerName = ([^,]+),([^-]+)-\d+/'; 
 
+$functor = new Functor();
+
+$file = new SplFileObject();
+
+foreach($file as $no =< $line) {
+
+  if (preg_match($regex, $line, $matches) !== 1) 
+     continue;
+  
+  $functor($matches);
+}
